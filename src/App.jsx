3f886ab06959895eb370dca7dafe9780cfff1ca1
Manish Kumar Prasad <manishraz32@gmail.com>
2025-05-12 13:26:13 +0530

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import FeatureSection from "./components/FeatureSection/FeatureSection";
 import OurTeam from "./components/OurTeam/OurTeam";
 import ProjectSection from "./components/ProjectSection/ProjectSection";
+import { Search, Check } from "lucide-react";
+import Disnmept from "./assets/DisNmept.png";
+import google from "./assets/google.png";
+import HubSpot_ from "./assets/HubSpot_Logo.png";
+import Shopify from "./assets/Shopify_logo_2018.svg.png";
+import slack from "./assets/Slack_Technologies_Logo.svg.png";
+import YoutTube from "./assets/YouTube.svg.png";
+import image from "./assets/image.svg";
 
 function App() {
   return (
@@ -12,16 +17,17 @@ function App() {
       <div className="app">
         <nav className="navbar">
           <div className="navbar-list">
-            <a href="#nullBrains">nullBrains.</a>
+            <p className="nullBrains">nullBrains.</p>
             <div className="center-content">
+              <a href="#home">Home</a>
               <a href="#features">Features</a>
-              <a href="#pricing">Pricing</a>
               <a href="#blog">Blog</a>
               <a href="#contact">Contact</a>
+              <Search size={18} color="gray" />
             </div>
             <div className="login-and-singup">
-              <a href="#login">Login</a>
-              <button className="button">Sign</button>
+              <a href="#login">Sign In</a>
+              <button className="button">Sign Up</button>
             </div>
           </div>
         </nav>
@@ -30,12 +36,102 @@ function App() {
           <p className="start-craft-tex">Start Crafting Your</p>
           <p className="start-craft-text">Next Great Idea</p>
           <p className="description">
-            nullBrains is the solution to all those projects that never get
-            created. Stop wasting your time now and let's get started.
+            Simplifying the creation of landing pages, blog page, application
+            pages and so much more!
           </p>
           <div className="btn">
+            <div className="on-top-purchase">only $15/mo</div>
             <button className="primary">Purchase Now</button>
-            <button className="secondary">Learn More</button>
+            <p className="learn-more">Learn More</p>
+          </div>
+        </div>
+
+        <div className="logo_container">
+          <div className="trust_text">
+            <p>TRUSTED BY TOP_LEADING COMPAINES</p>
+          </div>
+          <div className="logo">
+            <img src={Disnmept} alt="DisNmept" />
+            <img src={google} alt="google" />
+            <img src={HubSpot_} alt="HubSpot" />
+            <img src={YoutTube} alt="YoutTube" />
+            <img src={slack} alt="slack" />
+            <img src={Shopify} alt="Shopify" />
+          </div>
+        </div>
+
+        <div className="third_section">
+          <div className="container">
+            <div className="frist_container">
+              <div className="image_section">
+                <img
+                  src={image}
+                  alt="Boost Productivity"
+                  className="top-image"
+                />
+                {/* <div></div> */}
+              </div>
+              <div className="p_container">
+                <h2>Boost Productivity</h2>
+                <p className="subtext">
+                  Build an atmosphere that fosters productivity in your
+                  organization and personal projects.
+                </p>
+                <ul className="points">
+                  <li>
+                    <span className="check-icon">
+                      <Check size={16} />
+                    </span>
+                    Streamline your productivity and workflow
+                  </li>
+                  <li>
+                    <span className="check-icon">
+                      <Check size={16} />
+                    </span>
+                    Build better task integrations
+                  </li>
+                  <li>
+                    <span className="check-icon">
+                      <Check size={16} />
+                    </span>
+                    Connect systems for a unified flow
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="second_container">
+              <div className="p_container">
+                <h2>Automated Tasks</h2>
+                <p className="subtext">
+                  Save time and resources with our intuitive automation system.
+                  No code required for setup.
+                </p>
+                <ul className="points">
+                  <li>
+                    <span className="check-icon">
+                      <Check size={16} />
+                    </span>
+                    Automate routine processes
+                  </li>
+                  <li>
+                    <span className="check-icon">
+                      <Check size={16} />
+                    </span>
+                    Connect systems for a unified flow
+                  </li>
+                  <li>
+                    <span className="check-icon">
+                      <Check size={16} />
+                    </span>
+                    Build custom scripts for your needs
+                  </li>
+                </ul>
+              </div>
+              <div className="image_section">
+                <img src={image} alt="Automated Tasks" />
+              </div>
+            </div>
           </div>
         </div>
 
--- src/components/ProjectSection/ProjectSection.jsx
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import "./ProjectSection.css";
+import Facebook from "lucide-react/dist/esm/icons/facebook";
+import Instagram from "lucide-react/dist/esm/icons/instagram";
+import Twitter from "lucide-react/dist/esm/icons/twitter";
+import Github from "lucide-react/dist/esm/icons/github";
+import Globe from "lucide-react/dist/esm/icons/globe";
+
 const ProjectSection = () => {
   return (
     <div className="project-container-section">
@@ -13,29 +19,77 @@ const ProjectSection = () => {
         <div className="top-cards-container">
           {[1, 2, 3].map(() => (
             <div className="project-card">
-            <div className="project-image-container">
-              <img
-                src="https://img.freepik.com/premium-photo/purple-aluminum-cans-with-water-droplets-purple-background_167862-6039.jpg?w=740"
-                className="project-image"
-              />
-            </div>
-            <div className="project-text-container">
-              <div className="chip-container">RESOURCES</div>
-              <div className="project-text-sub-container">
-                <div>
-                  <h1 className="project-card-text-header">Refreshing</h1>
-                  <h1 className="project-card-text-header negative-padding">Designs</h1>
-                </div>
-                <div className="project-card-desc">
-                  Quench satisfying designs to help you stear up emotion and
-                  tell a story
+              <div className="project-image-container">
+                <img
+                  src="https://img.freepik.com/premium-photo/purple-aluminum-cans-with-water-droplets-purple-background_167862-6039.jpg?w=740"
+                  className="project-image"
+                />
+              </div>
+              <div className="project-text-container borderRadiusBottom-16px">
+                <div className="chip-container">RESOURCES</div>
+                <div className="project-text-sub-container">
+                  <div>
+                    <h1 className="project-card-text-header">Refreshing</h1>
+                    <h1 className="project-card-text-header negative-padding">
+                      Designs
+                    </h1>
+                  </div>
+                  <div className="project-card-desc">
+                    Quench satisfying designs to help you stear up emotion and
+                    tell a story
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
           ))}
         </div>
-        <div></div>
+        <div>
+          <div className="top-cards-container">
+            {[1, 2, 3].map(() => (
+              <div className="project-card">
+                <div className="project-image-container-2">
+                  <img
+                    src="https://img.freepik.com/premium-photo/laptop-cellphone-headphone-with-cactus-plant-beige-background_23-2147889270.jpg?w=996"
+                    className="project-image borderTop-16px"
+                  />
+                </div>
+                <div className="project-text-container2 borderRadiusBottom-16px">
+                  <div className="chip-container2">RESOURCES</div>
+                  <div className="project-text-sub-container">
+                    <div className="project-text-header-2">
+                      Best workstations of the year
+                    </div>
+                    <div className="project-card-desc">
+                      Quench satisfying designs to help you stear up emotion and
+                      tell a story
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* foooter */}
+
+      <div className="footer-container">
+        <div className="link-container">
+          <div className="anchor-text">About</div>
+          <div className="anchor-text">Blog</div>
+          <div className="anchor-text">Team</div>
+          <div className="anchor-text">Pricing</div>
+          <div className="anchor-text">Contacts</div>
+          <div className="anchor-text">Terms</div>
+        </div>
+        <div className="icon-container2">
+          <Facebook />
+          <Instagram />
+          <Twitter />
+          <Github />
+          <Globe />
+        </div>
+        <div>@ 2021 SomeCompany, Inc, All right reserved</div>
       </div>
     </div>
   );
